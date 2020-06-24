14264a5e971bb45efb50e3d53c6fbc078c1b955b
Ajinkya Kulkarni <ajinkya@ajinkyakulkarni.com>
2020-06-24 18:29:07 +0000

    removed unused service definition

--- index.ts
+++ index.ts
@@ -5,18 +5,6 @@ import * as aws from "@pulumi/aws";
 const cluster = new awsx.ecs.Cluster("hls-lpdaac-orchestration");
 const img = awsx.ecs.Image.fromPath("hls-lpdaac-orchestration-img", "./script");
 
-const reconciliationService = new awsx.ecs.FargateService("reconciliation-service", {
-    cluster,
-    taskDefinitionArgs: {
-        container: {
-            image: img,
-            cpu: 102 /*10% of 1024*/,
-            memory: 50 /*MB*/,
-        },
-    },
-    desiredCount: 1,
-});
-
 //example ref - https://www.pulumi.com/docs/tutorials/aws/video-thumbnailer/
 const reconciliationTask = new awsx.ecs.FargateTaskDefinition("reconciliationTask", {
     container: {
