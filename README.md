78529c764a031aacd1a5f93cc985773a82ccc38a
EC2 Default User <ec2-user@ip-172-31-6-62.us-west-2.compute.internal>
2020-06-24 18:19:10 +0000

    initial pulumi stack

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-hls-lpdaac orchestration - a repository for creating and sending SNS messages with LPDAAC and for generating reconciliation reports.
+# HLS LPDAAC ORCHESTRATION
+
+This resposity contains HLS LPDAAC Orchestration Python Script, Docker container and Pulumi Deployment Script to run the docker container as Fargate task on a schedule
+
+## Installation Instructions
+* Install Node Version Manager (nvm)
+* Install Node.js using nvm
+* Install Docker and setup up appropriate permission to run Docker without sudo
+* Install https://www.pulumi.com/
+* Clone the repository
+* Login to pulumi `pulumi login --local`
+* Deploy the stack with command `pulumi up`
+
---
+++ index.ts
@@ -0,0 +1,39 @@
+import * as awsx from "@pulumi/awsx";
+import * as aws from "@pulumi/aws";
+
+
+const cluster = new awsx.ecs.Cluster("hls-lpdaac-orchestration");
+const img = awsx.ecs.Image.fromPath("hls-lpdaac-orchestration-img", "./script");
+
+const reconciliationService = new awsx.ecs.FargateService("reconciliation-service", {
+    cluster,
+    taskDefinitionArgs: {
+        container: {
+            image: img,
+            cpu: 102 /*10% of 1024*/,
+            memory: 50 /*MB*/,
+        },
+    },
+    desiredCount: 1,
+});
+
+//example ref - https://www.pulumi.com/docs/tutorials/aws/video-thumbnailer/
+const reconciliationTask = new awsx.ecs.FargateTaskDefinition("reconciliationTask", {
+    container: {
+        image: awsx.ecs.Image.fromPath("reconciliationTask", "./script"),
+        memoryReservation: 128,
+    },
+});
+
+const reconciliationScheduleHandler = async (event) => {
+    reconciliationTask.run({ cluster });
+};
+
+
+// More info on Schedule Expressions at
+// https://docs.aws.amazon.com/AmazonCloudWatch/latest/events/ScheduledEvents.html
+const reconciliationSchedule: aws.cloudwatch.EventRuleEventSubscription = aws.cloudwatch.onSchedule(
+    "reconciliationScheduleHandler",
+    "cron(0 12 * * ? *)",
+    reconciliationScheduleHandler,
+);
